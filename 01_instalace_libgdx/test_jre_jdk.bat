b4f65d1b6dee94ceff5c25b1bde1eeebec686d82
Rasovsky Zdenek <zdenek.rasovsky@gmail.com>
2019-04-05 23:33:51 +0200

    Složka 01
    Přidány soubory pro test instalace JDK a cti.txt.

---
+++ 01_instalace_libgdx/test_jre_jdk.bat
@@ -0,0 +1,18 @@
+@echo off
+echo *************************
+echo           JRE
+echo *************************
+
+java -version
+
+echo *                         
+
+echo *************************
+echo           JDK
+echo *************************
+
+javac -version
+
+echo *
+
+pause
